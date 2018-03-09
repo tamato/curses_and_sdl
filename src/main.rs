2f271fe508d97a0e987ddb9f6383502e08da0228
terry amato <terry.amato@mentice.com>
2018-03-09 10:38:26 -0700

    breaking them up some more

--- src/main.rs
+++ src/main.rs
@@ -177,9 +177,27 @@ impl SDLContext {
     }
 }
 
+use std::env;
+
 fn main() {
+
+    let mut use_curses = false;
+    for arguments in env::args() {
+        match arguments.as_str() {
+            "-t" => use_curses = true,
+            _ => {},
+        }
+        println!("arg: {:?}", arguments);
+    }
+
     let _sdl_ctx = SDLContext::new();
     let _curses_ctx = CursesContext::new();
-    let console_ctx = Box::new(&_curses_ctx as &ConsoleContext);
+    let console_ctx;
+    if use_curses {
+        console_ctx = Box::new(&_curses_ctx as &ConsoleContext);
+    } else {
+        console_ctx = Box::new(&_sdl_ctx as &ConsoleContext);
+    }
+
     console_ctx.do_everything();
 }
