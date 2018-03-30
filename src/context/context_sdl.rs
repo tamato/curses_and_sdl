33512346a3670f3c6f1969d06888721ed53b777d
terence amato <terence.amato@gmail.com>
2018-03-30 12:02:54 -0600

    code to use the new fonts, window size vs dungeon is mismatched

--- src/context/context_sdl.rs
+++ src/context/context_sdl.rs
@@ -7,14 +7,18 @@ use self::sdl2::keyboard::Keycode;
 use self::sdl2::rect::Rect;
 use self::sdl2::rect::Point;
 
+use self::sdl2::image::{LoadTexture, INIT_PNG};
+
 use context::ConsoleContext;
 use map_gen::{MapTileType, tcod_tutorial};
 
 use std::cmp::{max, min};
 
-const WINDOW_WIDTH: u32 = 640;
-const WINDOW_HEIGHT: u32 = 480;
-const TILE_SIZE: i32 = 32;
+const TILE_WIDTH: i32 = 12;
+const TILE_HEIGHT: i32 = 22;
+
+const WINDOW_WIDTH: u32 = 53 * TILE_WIDTH as u32;
+const WINDOW_HEIGHT: u32 = 22 * TILE_HEIGHT as u32;
 
 pub struct SDLContext {}
 impl ConsoleContext for SDLContext {
@@ -23,10 +27,15 @@ impl ConsoleContext for SDLContext {
         let video_subsystem = sdl_context.video().expect("Failed to get video_subsystem");
 
         let window = video_subsystem.window("SDL2", WINDOW_WIDTH, WINDOW_HEIGHT)
-            .position_centered().build().expect("Failed to acquire window from video_subsystem");
+            .position_centered()
+            .build()
+            .expect("Failed to acquire window from video_subsystem");
 
         let mut canvas = window.into_canvas()
-            .accelerated().build().expect("Failed to aqcuire canvas");
+            .accelerated()
+            .build()
+            .expect("Failed to aqcuire canvas");
+
         let texture_creator = canvas.texture_creator();
 
         canvas.set_draw_color(sdl2::pixels::Color::RGBA(0,0,0,255));
@@ -35,16 +44,19 @@ impl ConsoleContext for SDLContext {
 
         let mut event_pump = sdl_context.event_pump().expect("Failed to get event_pump");
 
-        let surface64x64 = sdl2::surface::Surface::load_bmp(Path::new("assets/tiles/32x32-less-noise.bmp")).expect("Failed to load 64x64");
-        let texture64 = texture_creator.create_texture_from_surface(&surface64x64).expect("Failed to get 64 texture");
-        let tile_size = 32;
+        let _image_context = sdl2::image::init(INIT_PNG).expect("failed to get image context");
 
-        let win_width = ((WINDOW_WIDTH) / tile_size) as usize;
-        let win_height = ((WINDOW_HEIGHT) / tile_size) as usize;
+        let texture22 = texture_creator.load_texture(Path::new("assets/font22.png"))
+            .expect("Failed to load 22x22");
+        let tile_width = TILE_WIDTH as u32;
+        let tile_height = TILE_HEIGHT as u32;
+
+        let win_width = ((WINDOW_WIDTH) / tile_width) as usize;
+        let win_height = ((WINDOW_HEIGHT) / tile_height) as usize;
         let map = tcod_tutorial::map_generation(win_width, win_height);
         let tiles = TILE::new();
 
-        let player_rect = Rect::new(TILE_SIZE * 19, TILE_SIZE * 3, TILE_SIZE as u32, TILE_SIZE as u32);
+        let player_rect = Rect::new(TILE_WIDTH * 0, TILE_HEIGHT * 4, TILE_WIDTH as u32, TILE_HEIGHT as u32);
         let mut player_x = 5 as i32;
         let mut player_y = 5 as i32;
 
@@ -79,14 +91,14 @@ impl ConsoleContext for SDLContext {
                     let width = *w as usize;
                     let y = idx / width;
                     let x = idx % width;
-                    let dest = Rect::new( (x * tile_size as usize) as i32, (y * tile_size as usize) as i32, tile_size, tile_size);
+                    let dest = Rect::new( (x * tile_width as usize) as i32, (y * tile_width as usize) as i32, tile_width, tile_height);
 
-                    canvas.copy_ex(&texture64, Some(tiles[ *tile ]), Some(dest), 0.0, None, false, false).expect("Failed to set map");
+                    canvas.copy_ex(&texture22, Some(tiles[ *tile ]), Some(dest), 0.0, None, false, false).expect("Failed to set map");
                 }
             });
 
-            let player_dest = Rect::new(player_x * TILE_SIZE, player_y * TILE_SIZE, TILE_SIZE as u32, TILE_SIZE as u32);
-            canvas.copy_ex(&texture64, Some(player_rect), Some(player_dest), 0.0, None, false, false).expect("Failed to load player");
+            let player_dest = Rect::new(player_x * TILE_WIDTH, player_y * TILE_HEIGHT, TILE_WIDTH as u32, TILE_HEIGHT as u32);
+            canvas.copy_ex(&texture22, Some(player_rect), Some(player_dest), 0.0, None, false, false).expect("Failed to load player");
 
             canvas.present();
             ::std::thread::sleep(Duration::from_millis(100));
@@ -110,10 +122,10 @@ struct TILE {
 impl TILE {
     fn new() -> Self {
         TILE {
-            floor: Rect::new( 0, TILE_SIZE * 23, TILE_SIZE as u32, TILE_SIZE as u32),
-            wall: Rect::new( 0, TILE_SIZE * 22, TILE_SIZE as u32, TILE_SIZE as u32),
-            chasm: Rect::new( 0, TILE_SIZE * 23, TILE_SIZE as u32, TILE_SIZE as u32),
-            water: Rect::new( 0, TILE_SIZE * 23, TILE_SIZE as u32, TILE_SIZE as u32),
+            floor: Rect::new( TILE_WIDTH *  7, TILE_HEIGHT *  0, TILE_WIDTH as u32, TILE_HEIGHT as u32),
+            wall:  Rect::new( TILE_WIDTH * 11, TILE_HEIGHT * 13, TILE_WIDTH as u32, TILE_HEIGHT as u32),
+            chasm: Rect::new( TILE_WIDTH *  0, TILE_HEIGHT *  0, TILE_WIDTH as u32, TILE_HEIGHT as u32),
+            water: Rect::new( TILE_WIDTH *  7, TILE_HEIGHT * 15, TILE_WIDTH as u32, TILE_HEIGHT as u32),
         }
     }
 }
